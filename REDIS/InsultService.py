70a2d84ae9f50fd5891f059fd7af95b559f33ed5
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-03-26 13:34:58 +0100

    InsultService.py for the Redis Middleware

--- REDIS/InsultService.py
+++ REDIS/InsultService.py
@@ -1,13 +1,38 @@
 import redis
 import time
+import random
+import multiprocessing
 
 client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 listName = "listInsults"
 setList = "setInsults"
+news = "news_channel"
+proc = None
+
+def activateBroadcast():
+    if (proc is None) or (not proc.is_alive()):
+        proc = multiprocessing.Process(target=broadcaster)
+        proc.start()
+
+def deactivateBroadcast():
+    if ((proc is not None) and (proc.is_alive())):
+        proc.terminate()
+
+def broadcaster():
+    while True:
+        choice = random.choice(client.lrange(setList,0,-1))
+        client.lpush(news,choice)
+        time.sleep(5)
 
 while True:
     task = client.lpop(listName)
     if task is not None:
-        client.sadd(setList,task)
-        time.sleep(3)
+        match task[0]:
+            case 1:
+                activateBroadcast()
+            case 2:
+                deactivateBroadcast()
+            case _:
+                client.sadd(setList,task)
+    time.sleep(3)
