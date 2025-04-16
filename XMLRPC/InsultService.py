ac96dd35aa5891edbb3701e043fc3d7d8455c73f
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-04-16 23:09:08 +0200

    Resolved exceptions and more info of what is happening

--- XMLRPC/InsultService.py
+++ XMLRPC/InsultService.py
@@ -22,39 +22,49 @@ with SimpleXMLRPCServer(('localhost',8000),requestHandler=RequestHandler,allow_n
         listObservers = manager.list()
 
         def add_insult(self, insult):
+            print(f"Received Insult : {insult}")
             if insult not in self.listInsult:
                 self.listInsult.append(insult)
+                return print(f"Added Insult : {insult}")
 
         def get_insults(self):
             return list(self.listInsult)
         
+        def get_insult(self):
+            return random.choice(self.listInsult)
+        
         def broadcaster(self):
             while True:
-                print(self.listInsult)
-                choice = random.choice(self.listInsult)
-                for ob in self.listObservers:
-                    observer = xmlrpc.client.ServerProxy(f"{ob}")
-                    observer.notify(choice)
-                    # print(f"{choice}")
-                time.sleep(5)
+                if len(self.listInsult) != 0:
+                    choice = random.choice(self.listInsult)
+                    for ob in self.listObservers:
+                        observer = xmlrpc.client.ServerProxy(f"{ob}")
+                        observer.notify(choice)
+                        # print(f"Sended the insult {choice} to the observer{ob}")
+                    time.sleep(5)
         
         def activateBroadcast(self):
             if (self.proc is None) or (not self.proc.is_alive()):
                 self.proc = multiprocessing.Process(target=self.broadcaster)
                 self.proc.start()
+            return "The Broadcast has been activated"
 
         def deactivateBroadcast(self):
             if (self.proc is not None) and (self.proc.is_alive()):
                 self.proc.terminate()
+            return "The Broadcast has been deactivated"
 
         def addObserver(self,url):
-            self.listObservers.append(url)
-            return print(f"This {url} has been added as a suscriber")
+            if url not in self.listObservers:
+                self.listObservers.append(url)
+            return f"This {url} has been added as a suscriber"
         
         def deleteObserver(self,url):
-            self.listObservers.remove(url)
-            return print(f"This {url} has been deleted as a suscriber")
+            if url in self.listObservers:
+                self.listObservers.remove(url)
+            return f"This {url} has been deleted as a suscriber"
 
-    server.register_instance(InsultServer()) 
+    instance = InsultServer()
+    server.register_instance(instance) 
     server.serve_forever()
     
