4449351a7adc62cb4eb8d2fd4405156dd8ad6e4c
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-04-16 23:14:44 +0200

    Without insults in another package

--- REDIS/InsultFilter.py
+++ REDIS/InsultFilter.py
@@ -1,14 +1,24 @@
 import redis
-import sys
-sys.path.insert(0, '../')
-from insults import listInsults
+# import sys
+# import os
+# pwd = os.getcwd()
+# print(f'{pwd}/../')
+# sys.path.insert(0, '/home/iulian-sebastian-oprea/Documents/sd/task1/task1_sd/')
+# from insults import listInsults
+
+insult = [
+    "cavero",
+    "asshole",
+    "dumb",
+    "motherfucker"
+]
 
 client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 work_queue = "work_queue"
 
 def censore(message):
     listCensored="listCensored"
-    for ins in listInsults.insult:
+    for ins in insult:
         if ins in message:
             message = message.replace(ins,"CENSORED")
     client.lpush(listCensored,message) 
