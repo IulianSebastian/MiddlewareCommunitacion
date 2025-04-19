c81a340ce622498f9bf55fe79e83ec2330ec4d5d
Iulian <93289638+IulianSebastian@users.noreply.github.com>
2025-04-19 21:57:17 +0200

    Create InsultFilter.py

---
+++ REDIS/multiple_node/filter/InsultFilter.py
@@ -0,0 +1,24 @@
+import redis
+
+insult = [
+    "cavero",
+    "asshole",
+    "dumb",
+    "motherfucker"
+]
+
+client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+work_queue = "work_queue"
+
+def censore(message):
+    listCensored="listCensored"
+    for ins in insult:
+        if ins in message:
+            message = message.replace(ins,"CENSORED")
+    print(message)
+    client.lpush(listCensored,message) 
+
+while True:
+    task = client.blpop(work_queue,timeout=0)
+    if task:
+        censore(task[1])
