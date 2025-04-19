0f8ed73bd2bb3a9f35e52aa4fb1b33bdbd680f30
Iulian <93289638+IulianSebastian@users.noreply.github.com>
2025-04-19 21:54:00 +0200

    Create InsultClientService.py

---
+++ REDIS/single_node/InsultClientService.py
@@ -0,0 +1,20 @@
+import sys
+import redis
+import random
+
+insults = [
+    "cavero",
+    "asshole",
+    "dumb",
+    "motherfucker"
+]
+
+client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+insultChannel="insultChannel"
+choice = None
+if sys.argv[1] == "0":
+    choice = random.choice(insults)
+else:
+    choice = sys.argv[1] 
+client.publish(insultChannel,choice)
+print(choice)
