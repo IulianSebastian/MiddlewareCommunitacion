85bbf50e24ff962f7f2a3566e7760162d533b7a3
Iulian <93289638+IulianSebastian@users.noreply.github.com>
2025-04-19 22:08:10 +0200

    Create InsultFilterWorker.py

---
+++ XMLRPC/multiple_node/InsultFilterWorker.py
@@ -0,0 +1,39 @@
+# xmlrpc IMPORT
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client
+import sys
+
+insults = [
+    "cavero",
+    "asshole",
+    "dumb",
+    "motherfucker"
+]
+
+portServer = int(sys.argv[1])
+portWorker = int(sys.argv[2])
+
+s = xmlrpc.client.ServerProxy(f'http://localhost:{portServer}')
+s.add_worker(portWorker)
+
+class RequestHandler(SimpleXMLRPCRequestHandler): 
+    rpc_paths = ('/RPC2')
+
+with SimpleXMLRPCServer(('localhost',portWorker),requestHandler=RequestHandler,allow_none=True) as server:
+    server.register_introspection_functions()
+
+    class InsultFilterWorker:
+        phrases = []
+
+        def get_insults(self):
+            return self.phrases
+
+        def work(self, phrase):
+            for insult in insults:
+                if insult in phrase:
+                    self.phrases.append(phrase.replace(insult,"CENSORED"))
+
+    server.register_instance(InsultFilterWorker()) 
+    server.serve_forever()
+    
