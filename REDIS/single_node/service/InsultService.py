f6f63f12410c253ec744286be06f58b85268c982
Iulian <93289638+IulianSebastian@users.noreply.github.com>
2025-04-19 21:51:47 +0200

    Create InsultService.py

---
+++ REDIS/single_node/service/InsultService.py
@@ -0,0 +1,45 @@
+import redis
+import time
+import random
+import multiprocessing
+
+client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+setList = "setInsults"
+insultChannel= "insultChannel"
+observerChannel = "newsChannel"
+proc = None
+
+# Methods for the Broadcast Utility
+def activateBroadcast():
+    global proc
+    if (proc is None) or (not proc.is_alive()):
+        proc = multiprocessing.Process(target=broadcaster)
+        proc.start()
+
+def deactivateBroadcast():
+    if ((proc is not None) and (proc.is_alive())):
+        proc.terminate()
+
+def broadcaster():
+    while True:
+        insultlist = client.smembers(setList)
+        if insultlist:
+            choice = random.choice(list(insultlist))
+            client.publish(observerChannel,choice)
+        time.sleep(5)
+
+# InsultService request and activate/deactivate.Broadcast
+pubsub = client.pubsub()
+pubsub.subscribe(insultChannel)
+
+for message in pubsub.listen():
+    if message["type"] == "message":
+        match message["data"][0]:
+            case "1":
+                activateBroadcast()
+            case "2":
+                deactivateBroadcast()
+            case _:
+                print(message["data"])
+                client.sadd(setList,message["data"])
