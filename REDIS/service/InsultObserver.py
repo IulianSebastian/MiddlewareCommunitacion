3f250aeb7d7d8d998eeee82722d2ca7dc91c8a4d
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-05-04 01:23:41 +0200

    Implemented insultme and listInsults

--- REDIS/service/InsultObserver.py
+++ REDIS/service/InsultObserver.py
@@ -1,6 +1,7 @@
 #python3 InsultObserver ObserverChannel
 import redis
 import time
+import json
 import sys
 
 client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
@@ -11,4 +12,4 @@ pubsub.subscribe(observerChannel)
 
 for message in pubsub.listen():
     if message["type"] == "message":
-        print(f"The message {message['data']}")
+        print(f"The message {json.loads(message['data'])["result"]}")
