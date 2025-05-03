34a87e8608f72e89326111b52c4e15f1bbf8990d
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-05-04 01:25:04 +0200

    Client adapted to work with last versions of filter and service

--- REDIS/InsultClient.py
+++ REDIS/InsultClient.py
@@ -1,24 +1,20 @@
-#python3 InsultClient.py where_to_send option( 0 -> Random Insult 1-> Activate Broadcast 2 -> Deactivate Broadcast [1 and 2 only for service]) (service or filter)
+# python3 InsultClient.py (service or filter) / where_to_send / petition / queue 
 import sys
 import redis
-import random
+import json
 
-insults = [
-    "cavero",
-    "asshole",
-    "dumb",
-    "motherfucker"
-]
+client=redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+send=sys.argv[2]
+choice=json.dumps({"pet":sys.argv[3],"queue":sys.argv[4]})
 
-client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
-send=sys.argv[1]
-choice = None
-if sys.argv[2] == "0":
-    choice = random.choice(insults)
-else:
-    choice = sys.argv[2] 
-
-if sys.argv[3] == "service":
+if sys.argv[1] == "service":
     client.publish(send,choice)
-elif sys.argv[3] == "filter":
+elif sys.argv[1] == "filter":
     client.rpush(send,choice)
+
+if sys.argv[4] != "0":
+    while True:
+        result = client.blpop(sys.argv[4],timeout=0)
+        if result:
+            print(list((json.loads(result[1]))["result"]))
+            break
