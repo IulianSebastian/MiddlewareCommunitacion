3914e51d462302cb9fca74f51d2e5702bb3222a5
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-05-04 01:24:42 +0200

    Adapted to work with the new version of service

--- REDIS/filter/InsultFilter.py
+++ REDIS/filter/InsultFilter.py
@@ -1,36 +1,42 @@
-#python3 InsultFilter.py Queue_where_to_work
+#python3 InsultFilter.py queue_where_to_work channel_service unique_queue_where_service_wil_send_message
 import redis
+import json
 import sys
-
-# insults = [
-#     "cavero",
-#     "asshole",
-#     "dumb",
-#     "motherfucker"
-# ]
 
 client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 work_queue = sys.argv[1]
-n_peticions = 0
+channel = sys.argv[2]
+queue = sys.argv[3]
+listCensored="listCensored"
 
 def censore(message):
-    listCensored="listCensored"
-    insults = client.smembers("setInsults")
+    client.publish(channel,json.dumps({"pet":"3","queue":queue}))
+
+    insults = None
+    while insults is None:
+        result = client.blpop(queue,timeout=0)
+        if result:
+            insults=list((json.loads(result[1]))["result"])
+
     for ins in insults:
         if ins in message:
             message = message.replace(ins,"CENSORED")
-    print(message)
-    # client.lpush(listCensored,message)
-    # client.incr('counter')
+
     with client.pipeline() as pipe:
         pipe.multi()
         client.lpush(listCensored,message)
         pipe.incr('counter')
         pipe.execute()
 
+def listInsults(queue):
+    client.rpush(queue,json.dumps({"result":list(client.lrange(listCensored,0,-1))}))
 
 while True:
     task = client.blpop(work_queue,timeout=0)
     if task:
-        censore(task[1])
+        msg = json.loads(task[1])
+        if msg["pet"] == "3":
+            listInsults(msg["queue"])
+        else:
+            censore(message=msg["pet"])
 
