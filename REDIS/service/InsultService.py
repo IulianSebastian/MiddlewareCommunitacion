5a47b9a855db7099ef186f3f6fabab9545b770d7
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-05-04 01:24:08 +0200

    From anteiror commit

--- REDIS/service/InsultService.py
+++ REDIS/service/InsultService.py
@@ -3,6 +3,7 @@ import multiprocessing
 import random
 import redis
 import time
+import json
 import sys
 
 insultChannel = sys.argv[1]
@@ -12,7 +13,6 @@ client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 setList = "setInsults"
 proc = None
 
-# Methods for the Broadcast Utility
 def activateBroadcast():
     global proc
     if (proc is None) or (not proc.is_alive()):
@@ -28,26 +28,33 @@ def broadcaster():
         insultlist = client.smembers(setList)
         if insultlist:
             choice = random.choice(list(insultlist))
-            client.publish(observerChannel, choice)
+            client.publish(observerChannel, json.dumps({"result":choice}))
         time.sleep(5)
 
-# InsultService request and activate/deactivate.Broadcast
+def listInsults(queue):
+    print(json.dumps({"result":list(client.smembers(setList))}))
+    client.rpush(queue,json.dumps({"result":list(client.smembers(setList))}))
+
+def insult_me(queue):
+    client.rpush(queue,json.dumps({"result":random.choice(list(client.smembers(setList)))}))
+
 pubsub = client.pubsub()
 pubsub.subscribe(insultChannel)
 
 for message in pubsub.listen():
     if message["type"] == "message":
-        match message["data"][0]:
-            case "1":
-                print("Activating broadcast")
-                activateBroadcast()
-            case "2":
-                print("Deactivating broadcast")
-                deactivateBroadcast()
-            case _:
-                print(message["data"])
-                with client.pipeline() as pipe:
-                    pipe.multi()
-                    pipe.sadd(setList, message["data"])
-                    pipe.incr('counter')
-                    pipe.execute()
+        msg = json.loads(message["data"])
+        if msg["pet"] == "1":
+            activateBroadcast()
+        elif msg["pet"] == "2":
+            deactivateBroadcast()
+        elif msg["pet"] == "3":
+            listInsults(msg["queue"])
+        elif msg["pet"] == "4":
+            insult_me(msg["queue"])
+        else:
+            with client.pipeline() as pipe:
+                pipe.multi()
+                pipe.sadd(setList, msg["pet"])
+                pipe.incr('counter')
+                pipe.execute()
